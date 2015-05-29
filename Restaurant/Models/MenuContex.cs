4454fb00a10dfe22ddf8708c2b0301c41c9518b7
alexkaduk <o.kaduk@gmail.com>
2015-05-29 18:37:59 +0200

    Begining

---
+++ Restaurant/Models/MenuContex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Restaurant.Models;
+
+namespace Restaurant.Models
+{
+    public class MenuContex : DbContext 
+    {
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<MenuCard> MenuCards { get; set; }
+    }
+}
---
+++ Restaurant/Models/MenuPlanner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MenuPlanner.Models
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public decimal Price { get; set; }
+        public int Active { get; set; }
+        public int Order { get; set; }
+        public int Type { get; set; }
+        public DateTime? Day { get; set; }
+
+        public int MenuCardId { get; set; }
+        public virtual MenuCard MenuCard { get; set; }
+    }
+
+    public class MenuCard
+    {
+        // https://msdn.microsoft.com/ru-ru/data/jj193542.aspx
+        // https://www.youtube.com/watch?v=nDYU9gYYAHY
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Active { get; set; }
+        public int Order { get; set; }
+        public virtual ICollection<Menu> Menus { get; set; }
+    }
+}
